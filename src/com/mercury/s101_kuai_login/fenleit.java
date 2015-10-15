01ca4d3db831fe0278a462a2e0fce49e1b3a3b3a
mercury <183941826@qq.com>
2015-10-15 11:10:51 +0800

    完成了分类页面的视图
    Signed-off-by: mercury <183941826@qq.com>

--- src/com/mercury/s101_kuai_login/fenleit.java
+++ src/com/mercury/s101_kuai_login/fenleit.java
@@ -1,21 +1,34 @@
 package com.mercury.s101_kuai_login;
-
-
-
-
 import com.mercury.s101_kuai.R;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class fenleit extends Fragment{
+public class fenleit extends Fragment implements View.OnClickListener{
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fenlei, container, false);
+		View v = inflater.inflate(R.layout.fenlei, container, false);
+		Button appliance = (Button) v.findViewById(R.id.fenleiappliance);
+		Button book = (Button) v.findViewById(R.id.fenleibook);
+		Button cloth = (Button) v.findViewById(R.id.fenleicloth);
+		Button compute = (Button) v.findViewById(R.id.fenleicompute);
+		Button digitcamer = (Button) v.findViewById(R.id.fenleidigtcamer);
+		Button furniture = (Button) v.findViewById(R.id.fenleifurnitrue);
+		Button mobile = (Button) v.findViewById(R.id.fenleimobile);
+		Button skincare = (Button) v.findViewById( R.id.fenleiskincare);
+		return v;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		
 	}
 }
--- src/com/mercury/s101_kuai_login/wodet.java
+++ src/com/mercury/s101_kuai_login/wodet.java
@@ -1,19 +1,79 @@
 package com.mercury.s101_kuai_login;
 
-import com.mercury.s101_kuai.R;
-
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class wodet extends Fragment{
+import com.mercury.s101_kuai.R;
 
+public class wodet extends Fragment implements View.OnClickListener{
+	private Button wodeselect;
+	private Button wodepayfor;
+	private Button wodeallgoods;
+	private Button wodegoods;
+	private Button wodemessage;
+	private Button wodeattention;
+	private Button wodeabout;
+	private Button wodeadvice;
+	private Button wodequit;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.wode, container, false);
+		View v = inflater.inflate(R.layout.wode, container, false);
+		wodeselect = (Button) v.findViewById(R.id.wodeSelect);
+		wodepayfor = (Button) v.findViewById(R.id.wodepayfor);
+		wodeallgoods = (Button) v.findViewById(R.id.wodeallgoods);
+		wodemessage = (Button) v.findViewById(R.id.wodemessage);
+		wodegoods = (Button) v.findViewById(R.id.wodegoods);
+		wodeattention = (Button) v.findViewById(R.id.wodeattention);
+		wodeabout = (Button) v.findViewById(R.id.wodeabout);
+		wodeadvice = (Button) v.findViewById(R.id.wodeadvice);
+		wodequit = (Button) v.findViewById(R.id.wodequit);
+		return v;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.wodeSelect:
+			System.out.println();
+			Toast.makeText(getActivity(), "shousuo", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.wodepayfor:
+			
+			break;
+		case R.id.wodeallgoods:
+			
+			break;
+		case R.id.wodegoods:
+			
+			break;
+		case R.id.wodemessage:
+			
+			break;
+		case R.id.wodeattention:
+			
+			break;
+		case R.id.wodeabout:
+			
+			break;
+		case R.id.wodeadvice:
+			
+			break;		
+		case R.id.wodequit:
+				
+			break;
+				
+			
+			
+
+		default:
+			break;
+		}
+		
 	}
 }
